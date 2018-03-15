ccb61d5dbfd5767b8b6d94599c3e8c44216a073a
Dylan McCapes <dylanmccapes@Dylans-MacBook-Pro.local>
2018-03-15 10:45:05 -0700

    Favicon works locally

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express'
-import favicon from 'express-favicon'
+import favicon from 'serve-favicon'
 import path from 'path'
 import open from 'open'
 import webpack from 'webpack'
-import config from '../webpack.config.dev'
+import config from './webpack.config.dev'
 
 const compiler = webpack(config)
 const port = 3000;
 const app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
@@ -25,6 +25,6 @@ app.listen(port, function (error) {
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'))
+    res.sendFile(path.join(__dirname, './src/index.html'))
 });
 
